9eda66aafcdfc29a12cebcffed08f9029f31cd1a
vekkt <francois.burguet@epfl.ch>
2021-05-04 16:26:07 +0200

    added callable individual pricing

--- Test.py
+++ Test.py
@@ -16,18 +16,22 @@ beta  = 0.8
 r, y, u, d = calibration()
 tree = Binomial(r, T, dt, i0, u, d, y)
 
-verbose = False
+print('{:10} : {:.4f}'.format('  rcn', tree.price_RCN(alpha, c)))
+print('{:10} : {:.4f}'.format(' brcn', tree.price_RCN(alpha, c, beta)))
 
-tree = Binomial(r, T, dt, i0, u, d, y)
+print('{:10} : {:.4f}'.format(' crcn', tree.price_RCN(alpha, c, callable=True)))
+print('{:10} : {:.4f}'.format('cbrcn', tree.price_RCN(alpha, c, beta, callable=True)))
 
-print('{:10} : {:.4f}'.format(' rcn', tree.price_RCN(alpha, c)))
-print('{:10} : {:.4f}'.format('brcn', tree.price_RCN(alpha, c, beta)))
-print('{:10} : {:.4f}'.format('bond', tree.price_bond(c*dt)))
 
 print()
 note = rcn(r, dt, i0, y, u, d, c, T)
 print('{:10} : {:.4f}'.format(' rcn', note.price_rcn(alpha=alpha, c=c)))
-print(note.price_rcn(alpha=alpha, c=c, replication_strategy=True))
 print('{:10} : {:.4f}'.format(
     'brcn', note.price_brcn(alpha=alpha, beta=beta, c=c)))
-print('{:10} : {:.4f}'.format('bond', note.bond))
+dates = [j for j in range(1, T)]
+RCN, rep1 = note.price_rcn(
+    alpha=alpha, c=c, dates=dates, replication_strategy=True)
+BRCN, rep2 = note.price_brcn(
+    alpha=alpha, c=c, beta=0.8, dates=dates, replication_strategy=True)
+print('{:10} : {:.4f}\n{:10} : {:.4f}'.format('crcn',
+    RCN, 'cbrcn', BRCN))
--- binomial.py
+++ binomial.py
@@ -18,6 +18,8 @@ class Binomial():
         self.attr['q'] = q
         self.attr['ind'] = []
 
+###############################################################################
+
     def price_bond(self, c, fv=1, T=None):
         if T is None: T = self.attr['T']
         r = self.attr['r']
@@ -25,20 +27,26 @@ class Binomial():
         # We assume continuous compounding
         return sum([c * exp(-r * dt * t) for t in range(1, T-1)]) + (fv+c) * exp(-r * dt * (T-1))
 
-    def price_put(self, K, type='E', dates=None, pen=0., verbose=False):
-        return self.price(lambda s: put(s, K), type, dates, pen, verbose)
+    def price_put(self, K, type='KI', full=False, dates=None, pen=0.):
+        if not full:
+            return self.price(lambda s: put(s, K), type, dates, pen)
+        return self.price_barrier(lambda s: put(s, K))
 
     def price_call(self, K, type='E', dates=None, pen=0.):
         return self.price(lambda s: call(s, K), type, dates, pen)
 
     def price_barrier_call(self, K, beta, type='KI'):
-        return self.price_barrier(lambda s: call(s, K), beta, type)
+        return self.price_barrier(lambda s: call(s, K), beta=beta, type=type)
 
-    def price_barrier_put(self, K, beta, type='KI', verbose=False):
-        return self.price_barrier(lambda s: put(s, K), beta, type, verbose)
+    def price_barrier_put(self, K, beta, type='KI'):
+        return self.price_barrier(lambda s: put(s, K), beta=beta, type=type)
 
 
-    def price(self, payoff, type='E', dates=None, pen=0., verbose=False):
+# Compute the price of a derivative with the given payoff
+# Using recombining trees
+###############################################################################
+
+    def price(self, payoff, type='E', dates=None, pen=0.):
         r, T, dt, S0, y, u, d, q, _= self.attr.values()
         if type == 'B':
             assert(len(dates) >= 1 and (T-1) in dates)
@@ -73,28 +81,20 @@ class Binomial():
 
         self.underlying_recomb = s_ex
 
-        if verbose:
-            print('Underlying price')
-            print(s_ex)
-            print('Option price')
-            print(p)
         return p
 
-    def underlying_price(self, beta=None, verbose=False):
+
+
+# Compute the value of the underlying asset
+###############################################################################
+
+    def underlying_price(self, beta=None):
         r, T, dt, S0, y, u, d, q, _= self.attr.values()
 
         g = exp(-r*dt)
-        if q is None:
-            q = (1/g - d) / (u - d)
 
-        if beta is not None:
-            ind = []
-            # If the asset starts below the barrier, this is an Up option
-            if beta > 1:
-                type = 'U'
-            # If the asset starts below the barrier, this is a Down option
-            if beta <= 1:
-                type = 'D'
+        if q is None: q = (1/g - d) / (u - d)
+        if beta is not None: ind = []
 
         s_ex = np.zeros((2**(T-1), T))
         s_ex[0, 0] = S0
@@ -114,8 +114,8 @@ class Binomial():
                 if beta is not None:
                     # We add all leaves that come from a node
                     # where the threshold has been breached
-                    if (type == 'D' and s_ex[i, j] <= beta * S0 or
-                            type == 'U' and s_ex[i, j] >= beta * S0):
+                    if (beta <= 1 and s_ex[i, j] <= beta * S0 or
+                            beta > 1 and s_ex[i, j] >= beta * S0):
                         # L is the number of leaves from this node
                         # i * L is the index of the first leave
                         L = 2**(T-1-j)
@@ -123,24 +123,28 @@ class Binomial():
 
         if beta is not None:
             for i in range(2**(T-1)):
-                if (type == 'D' and s_ex[i, T-1] <= beta * S0 or
-                        type == 'U' and s_ex[i, T-1] >= beta * S0):
+                if (beta <= 1 and s_ex[i, T-1] <= beta * S0 or
+                        beta > 1 and s_ex[i, T-1] >= beta * S0):
                     ind.append(i)
             # remove duplicates and save
             self.attr['ind'] = list(set(ind.copy()))
-        if verbose:
-            print('Underlying price')
-            print(s_ex)
+        
         return s_ex
 
-    def price_barrier(self, payoff, beta, type='KI',verbose=False):
-        self.underlying = self.underlying_price(beta, verbose)
-        
-        underlying = self.underlying
+
+
+# Compute the price of a derivative with the given payoff
+# Using recombining trees
+###############################################################################
+
+    def price_barrier(self, payoff, underlying=None, beta=None, type='KI', dates=None):
+        if underlying is None: 
+            self.underlying = self.underlying_price(beta)
+            underlying = self.underlying
+
         r, T, dt, _, _, u, d, q, ind = self.attr.values()
         g = exp(-r*dt)
-        if q is None:
-            q = (1/g - d) / (u - d)
+        if q is None:  q = (1/g - d) / (u - d)
 
         p = np.zeros_like(underlying)
 
@@ -151,45 +155,64 @@ class Binomial():
         # the barrier has been breached at some point
         for i in range(2**(T-1)):
             p[i, T-1] = payoff(underlying[i, T-1])
-            if type[1] == 'I' and i not in ind:
-                p[i, T-1] = 0
-            elif type[1] == 'O' and i in ind:
-                p[i, T-1] = 0
+            if beta is not None and type != 'B':
+                if type[1] == 'I' and i not in ind:
+                    p[i, T-1] = 0
+                elif type[1] == 'O' and i in ind:
+                    p[i, T-1] = 0
 
         # Populate backwards
         for j in reversed(range(T-1)):
             for i in range(2**j):
                 p[i, j] = g * (q*p[2*i, j+1] + (1-q)*p[2*i+1, j+1])
+                S = underlying[i,j]
 
-        if verbose:
-            print('Option price:')
-            print(p)
+                if type == 'B' and dates is not None and j in dates: # Bermudan
+                    p[i, j] = max(payoff(S), p[i, j])
+
         return p
 
-    def price_RCN(self, alpha, c, beta=None, callable=False, verbose=False):
+    def price_RCN(self, alpha, c, beta=None, callable=False):
         S0 = self.attr['s0']
         dt = self.attr['dt']
+        T  = self.attr['T']
         y  = self.attr['y']
-        K = alpha*S0
+        K  = alpha*S0
 
         bond = self.price_bond(c*dt)
+        payoff = lambda S: max(S-1, 0)
+        dates  = [i for i in range(1, T)]
         # Basic RCN
         if beta is None and not callable:
-            put = self.price_put(K,verbose=verbose)
+            put   = self.price_put(K)
             price = bond - put[0, 0] / S0
 
-            ud = self.underlying_recomb
-            pi1 = (put[0, 1]-put[1, 1])/((ud[0, 1]-ud[1, 1])/(1-y))
-            pf = [pi1, put[0,0] - pi1*S0]
+            # ud = self.underlying_recomb
+            # pi1 = (put[0, 1]-put[1, 1])/((ud[0, 1]-ud[1, 1])/(1-y))
+            # pf = [pi1, put[0,0] - pi1*S0]
 
         # Barrier RCN
         if beta is not None and not callable:
-            put = self.price_barrier_put(K, beta, type='KI',verbose=verbose)
+            put   = self.price_barrier_put(K, beta, type='KI')
             price = bond - put[0, 0] / S0
 
-            ud = self.underlying
-            pi1 = (put[0, 1] - put[1, 1]) / (S0*(ud[0, 1] - ud[1, 1]) / (1-y))
-            pf = [pi1, price-pi1*ud[0, 0]]
+            # ud = self.underlying
+            # pi1 = (put[0, 1] - put[1, 1]) / (S0*(ud[0, 1] - ud[1, 1]) / (1-y))
+            # pf = [pi1, price-pi1*ud[0, 0]]
+
+        # Callable RCN
+        if beta is None and callable:
+            put    = self.price_put(K, full=True)
+            rcn    = bond - put / S0
+            ber    = self.price_barrier(payoff, underlying=rcn, type='B', dates=dates)
+            price  = rcn[0, 0] - ber[0, 0]
+
+        # Callable Barrier RCN
+        if beta is not None and callable:
+            put    = self.price_barrier_put(K, beta, type='KI')
+            brcn   = bond - put / S0
+            ber    = self.price_barrier(payoff, underlying=brcn, type='B', dates=dates)
+            price  = brcn[0, 0] - ber[0, 0]
 
         return price
 
@@ -217,7 +240,6 @@ if __name__ == '__main__':
 
     tree = Binomial(r, T, dt, S0, u, d, y,q=0.5)
 
-    verbose = True
     print('{:10} : {:.4f}'.format(' rcn', tree.price_RCN(alpha, c)))
     print('{:10} : {:.4f}'.format('brcn', tree.price_RCN(alpha, c, beta)))
     print('{:10} : {:.4f}'.format('bond', tree.price_bond(c)))
